9ff98028fd1e3ffa043c11c709d5cc53b9c2508c
MnZrK <mnzrka@gmail.com>
2016-06-09 11:03:59 +0600

    added state machine for handling communication with server

--- FDCConsoleUI/Main.fs
+++ FDCConsoleUI/Main.fs
@@ -6,8 +6,7 @@ open System.Threading
 open FDCUtil.Main
 open FDCLogger
 open FDCNet
-
-let getBytes (str: string) = System.Text.Encoding.ASCII.GetBytes(str)
+open FDCDomain
 
 [<EntryPoint>]
 let main argv =
@@ -18,77 +17,8 @@ let main argv =
     let logger = new Logger() 
     logger.Info "FDCConsoleUI is starting! args: %A" argv
     
-    logger.Info "Starting Tcp client..."
-    let client = Dcpp.startClientAsync host port |> Async.RunSynchronously
-
-    try
-        let rawEvent = client.receivedEvent
-        let event = rawEvent |> Event.map System.Text.Encoding.ASCII.GetString
-
-        let readMessageAsync () = Async.AwaitEvent(event) 
-        
-        let rawWriteMsg = client.write 
-        let writeMessage (msg: string) = 
-            logger.Trace "Sending message...: %s" msg
-            rawWriteMsg (System.Text.Encoding.ASCII.GetBytes(msg)) 
-            logger.Trace "Message has been sent"
-                
-        event |> Event.add (fun message -> logger.Trace "Got message: %s" message)
-        
-        let dcppUnvalidatedEvent = event |> Event.choose (fun message -> 
-            match Dcpp.Message.parse message with
-            | None -> 
-                logger.Warn "Could not parse message: %s" message
-                None
-            | Some parsedMessage ->
-                logger.Trace "Parsed message: %A" parsedMessage
-                parsedMessage |> Some
-            )
-        let dcppEvent = dcppUnvalidatedEvent |> Event.choose (fun message -> 
-            match Dcpp.Message.validate message with
-            | Failure error -> 
-                logger.Error "Received message is not valid: %s" error
-                None
-            | Success validated ->
-                validated |> Some
-            )
-            
-        let readDcppMessageAsync () = Async.AwaitEvent(dcppEvent)
-        
-        async {
-            // lock message
-            let! lockResponse = readDcppMessageAsync()
-                       
-            let lockMsg = 
-                match lockResponse with
-                | Dcpp.Message.LockT lockMsg ->
-                    logger.Info "Got lock %A" lockMsg.lock
-                    logger.Info "Got pk %s" lockMsg.pk
-                    lockMsg
-                | _ -> failwith "Could not parse lock message"
-
-            // authorization
-            let nick = "MnZrKk"
-            let authRequest = Array.concat [getBytes "$Key "; Dcpp.Message.Lock.calculateKey lockMsg.lock; getBytes ("|$ValidateNick "+nick+"|") ]
-            rawWriteMsg authRequest
-            
-            let! authResponse = readDcppMessageAsync()
-            
-            match authResponse with
-            | Dcpp.Message.BadPassT _ -> failwith "Bad password"
-            | Dcpp.Message.GetPassT _ -> failwith "Password is not supported"
-            | Dcpp.Message.ValidateDeniedT _ -> failwith "Could not login, validate denied"
-            | Dcpp.Message.HelloT msg ->
-                if msg.nick <> Dcpp.Message.Hello.NickData nick then
-                    logger.Warn "Nick returned from server does not match our actual nick"
-                logger.Info "Logged in"
-            | _ -> failwith "Expected auth response" 
-                
-        } |> Async.RunSynchronously
-        
-        Thread.Sleep(10000)
-    finally
-        client.dispose()
+    let agent = Server.startServer { host = host; port = port } (Dcpp.Message.Hello.NickData "MnZrKk")
+    Thread.Sleep(5000)
 
     logger.Info "Shutting down..."
         
--- FDCDomain/Server.fs
+++ FDCDomain/Server.fs
@@ -1,10 +1,13 @@
 module FDCDomain.Server
 
+open System.Threading
+
 open FDCUtil.Main
 open FDCLogger
 open FDCNet
 open FDCNet.Util
 
+// TODO think about how to restrain transitions from certain states
 type State = 
 | NotConnected
 | Connected of Tcp.Client
@@ -18,12 +21,15 @@ type ConnectionInfo = {
 }
 
 // TODO add unit tests ?
+
+// active actions
 let connectAsync connectinfo state =
     let logger = new Logger() 
     match state with
     | NotConnected ->
         async {
             logger.Info "Connecting (%s:%i) ..." connectinfo.host connectinfo.port
+            // TODO check what happens when server is not available. exceptions are not propagated it seems
             let! client = Dcpp.startClientAsync connectinfo.host connectinfo.port
             logger.Info "Connected %s:%i" connectinfo.host connectinfo.port
             
@@ -31,15 +37,6 @@ let connectAsync connectinfo state =
         } |> Success
     | _ -> 
         "Already connected or connecting" |> Failure
-
-let lockAcquired (lockMsg: Dcpp.Message.Lock.T) state =
-    let logger = new Logger() 
-    match state with
-    | Connected client ->
-        logger.Info "Lock acquired"
-        LockAcquired (client, lockMsg.lock) |> Success
-    | _ -> 
-        "Invalid state for acquiring lock" |> Failure
 
 let sendNick nick state = 
     let logger = new Logger() 
@@ -55,8 +52,18 @@ let sendNick nick state =
         WaitingForAuth (client, lockData, nick) |> Success
     | _ ->
         "Invalid state for authorization" |> Failure
-            
-let loggedIn (helloMsg: Dcpp.Message.Hello.T) state =
+
+// passive reactions
+let onLockMessage (lockMsg: Dcpp.Message.Lock.T) state =
+    let logger = new Logger() 
+    match state with
+    | Connected client ->
+        logger.Info "Lock acquired"
+        LockAcquired (client, lockMsg.lock) |> Success
+    | _ -> 
+        "Invalid state for acquiring lock" |> Failure
+
+let onHelloMessage (helloMsg: Dcpp.Message.Hello.T) state =
     let logger = new Logger() 
     match state with
     | WaitingForAuth (client, lockData, sentNick) ->
@@ -69,3 +76,120 @@ let loggedIn (helloMsg: Dcpp.Message.Hello.T) state =
         LoggedIn (client, lockData, helloMsg.nick) |> Success
     | _ -> 
         "Invalid state for authorization" |> Failure
+
+let onBadPassMessage (msg: Dcpp.Message.BadPass.T) state =
+    let logger = new Logger()
+    match state with
+    | WaitingForAuth (client, lockData, sentNick) ->
+        let (Dcpp.Message.Hello.NickData nickStr) = sentNick
+        logger.Error "Invalid password for nick %s" nickStr
+
+        // server must close the connection right afterwards, but until then we set our state as ...
+        LockAcquired (client, lockData) |> Success
+    | _ ->
+        "Invalid state for authorization" |> Failure
+
+// action messages
+type Action =
+| Connect of ConnectionInfo
+| SendNick of Dcpp.Message.Hello.NickData
+| HandleMessage of Dcpp.Message.T
+
+let handleMessage message state = 
+    let logger = new Logger()
+
+    match message with
+    | Dcpp.Message.LockT msgData -> onLockMessage msgData state
+    | Dcpp.Message.BadPassT msgData -> onBadPassMessage msgData state
+    | Dcpp.Message.GetPassT msgData -> 
+        logger.Error "GetPass message is not supported yet"
+        "GetPass message is not supported yet" |> Failure
+    | Dcpp.Message.HelloT msgData -> onHelloMessage msgData state
+    | Dcpp.Message.ValidateDeniedT msgData -> 
+        logger.Error "ValidateDenied message is not supported yet"
+        "ValidateDenied message is not supported yet" |> Failure
+
+let dispatchAction action state =
+    let logger = new Logger()
+
+    match action with
+    | Connect connectInfo ->
+        match connectAsync connectInfo state with
+        | Failure s ->
+            logger.Fatal "Could not connect to %A" connectInfo
+            Failure (sprintf "Could not connect to %A" connectInfo)
+        | Success newStateAsync ->
+            let newState = newStateAsync |> Async.RunSynchronously
+            Success newState
+    | SendNick nick ->
+        sendNick nick state
+    | HandleMessage message ->
+        handleMessage message state
+
+let startServer connectInfo nick = 
+    let logger = new Logger()
+
+    // TODO make proper cancellation         
+    logger.Trace "Creating agent ..."
+    let agent = Agent.create NotConnected dispatchAction
+    agent.event |> Event.add (fun (state, state') -> 
+        logger.Info "Changing state from %A to %A" state state'
+    )
+
+    let establishedConnectionEvent = agent.event |> Event.choose (fun (state, state') ->
+        match (state, state') with
+        | NotConnected, Connected client ->
+            (state, state', client) |> Some
+        | _ -> 
+            None    
+    ) 
+    // TODO handle clearing of subscription on client.event when status becomes NotConnected
+    establishedConnectionEvent |> Event.add (fun (state, state', client) ->
+        logger.Info "Established connection"
+        let event = client.receivedEvent |> Event.map FDCNet.Util.getString
+        event |> Event.add (fun message -> logger.Trace "Got message: %s" message)
+        let dcppUnvalidatedEvent = event |> Event.choose (fun message -> 
+            match Dcpp.Message.parse message with
+            | None -> 
+                logger.Warn "Could not parse message: %s" message
+                None
+            | Some parsedMessage ->
+                logger.Trace "Parsed message: %A" parsedMessage
+                parsedMessage |> Some
+            )
+        let dcppEvent = dcppUnvalidatedEvent |> Event.choose (fun message -> 
+            match Dcpp.Message.validate message with
+            | Failure error -> 
+                logger.Error "Received message is not valid: %s" error
+                None
+            | Success validated ->
+                validated |> Some
+            )
+        dcppEvent |> Event.add (fun msg ->
+            agent.post <| HandleMessage msg
+        )
+    )
+
+    // TODO construct DSL so it looks like `apply Connected LockAquired fun` or something
+    agent.event |> Event.filter (
+        function
+        | Connected _, LockAcquired _ -> true
+        | _ -> false    
+    ) |> Event.add (fun _ ->
+        agent.post <| SendNick nick
+    )
+
+    logger.Info "Connecting (%A) ..." connectInfo
+    agent.post <| Connect connectInfo
+    agent
+    // let dispatcher message, state = 
+    //     match message with
+    //     | Dcpp.Message.LockT msgData -> onLockMessage msgData state
+    //     | Dcpp.Message.BadPassT msgData -> onBadPassMessage msgData state
+    //     | Dcpp.Message.GetPassT msgData -> 
+    //         logger.Error "GetPass message is not supported yet"
+    //             state
+    //     | Dcpp.Message.HelloT msgData -> onHelloMessage msgData state
+    //     | Dcpp.Message.ValidateDeniedT msgData -> 
+    //         logger.Error "ValidateDenied message is not supported yet"
+    //             state
