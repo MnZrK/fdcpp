e983d2ecf9891ecf07972156c388687449272a5c
MnZrK <mnzrka@gmail.com>
2016-06-18 08:20:59 +0600

    added Settings module and updated paket

--- FDCConsoleUI/Infrastructure.fs
+++ FDCConsoleUI/Infrastructure.fs
@@ -5,6 +5,7 @@ open System.Threading
 open System.Reactive.Subjects
 open System.Reactive.Concurrency
 open FSharp.Control.Reactive
+open FSharp.Configuration
 
 open FDCUtil.Main
 open FDCUtil.Main.Regex
@@ -141,3 +142,37 @@ let create_transport (connect_info: ConnectionInfo) =
         }
     )
 
+module Settings = 
+    type T = 
+        { hub_connection_info: ConnectionInfo
+        ; listen_info: ListenInfo
+        ; nick: NickData.T
+        ; pass_maybe: PasswordData.T option }
+
+    type private RawSettings = AppSettings<"App.config">
+
+    let create () = 
+        Result.success_workflow_with_string_failures {
+            let! hub_address = HostnameData.create RawSettings.HubAddress
+            let! hub_port = PortData.create RawSettings.HubPort
+            let hub_connection_info = 
+                { host = hub_address
+                ; port = hub_port }
+
+            let! listen_address = IpAddress.create RawSettings.ListenAddress
+            let! listen_port = PortData.create RawSettings.ListenPort
+            let listen_info = 
+                { ip = listen_address
+                ; port = listen_port }
+
+            let! nick = NickData.create RawSettings.Nickname
+            let! pass_maybe = 
+                if RawSettings.Password = String.Empty then Success None
+                else PasswordData.create RawSettings.Password |> Result.map Some
+            
+            return 
+                { hub_connection_info = hub_connection_info
+                ; listen_info = listen_info
+                ; nick = nick
+                ; pass_maybe = pass_maybe }            
+        }
--- FDCConsoleUI/Main.fs
+++ FDCConsoleUI/Main.fs
@@ -3,98 +3,12 @@ module FDCConcoleUI.Main
 open System
 open System.Threading
 
+open FSharp.Configuration
 open FSharp.Control.Reactive
 
 open FDCDomain.MessageQueue
 open FDCConsoleUI.Infrastructure
 open FDCUtil.Main
-
-// [<EntryPoint>]
-// let main argv =
-//     printfn "Starting... %A" Thread.CurrentThread.ManagedThreadId
-
-//     let observable = (Observable.publish << Observable.ofAsync) (async {
-//         printfn "Doing inside observable"
-//         do! Async.Sleep 2000
-//         return 10
-//     })
-//     use disposable_initial = Observable.connect observable
-
-//     // let observable = Observable.ofSeqOn System.Reactive.Concurrency.NewThreadScheduler.Default (seq {
-//     //     printfn "Doing inside observable %A" Thread.CurrentThread.ManagedThreadId
-//     //     Thread.Sleep 2000
-//     //     yield 10
-//     //     Thread.Sleep 2000
-//     //     yield 20
-//     // })
-//     // let observable = Builders.observe {
-//     //     Thread.Sleep 2000
-//     //     yield 10
-//     //     Thread.Sleep 2000
-//     //     yield 20
-//     // }
-
-//     use disposable = 
-//         observable
-//         |> (Observable.subscribeWithCallbacks 
-//             <| (fun x -> printfn "onNext1 %A %A" Thread.CurrentThread.ManagedThreadId x)
-//             <| (fun x -> printfn "onError1 %A %A" Thread.CurrentThread.ManagedThreadId x)
-//             <| (fun x -> printfn "onCompleted1 %A %A" Thread.CurrentThread.ManagedThreadId x))
-
-//     use disposable2 = 
-//         observable
-//         |> (Observable.subscribeWithCallbacks 
-//             <| (fun x -> printfn "onNext2 %A %A" Thread.CurrentThread.ManagedThreadId x)
-//             <| (fun x -> printfn "onError2 %A %A" Thread.CurrentThread.ManagedThreadId x)
-//             <| (fun x -> printfn "onCompleted2 %A %A" Thread.CurrentThread.ManagedThreadId x))
-
-//     printfn "Finished setup %A" Thread.CurrentThread.ManagedThreadId
-
-//     Thread.Sleep 5000
-
-//     printfn "Exiting..."
-//     0
-
-// [<EntryPoint>]
-// let main argv =
-//     printfn "Starting %A" Thread.CurrentThread.ManagedThreadId
-//     let eom_marker = Convert.ToByte '|'
-//     let client_result = TcpStreamClient.start_async eom_marker "localhost" 411 |> Async.RunSynchronously
-//     client_result
-//     |>! Result.mapFailure (fun e -> printfn "Error starting tcp client %A" e)
-//     |> Result.mapSuccess (fun client -> using client (fun _ -> 
-        
-//         // Thread.Sleep 2000
-
-//         printfn "Observable.add1 %A" Thread.CurrentThread.ManagedThreadId
-//         use disposable = 
-//             client.Received
-//             // |>! Observable.add (fun x -> printfn "Observable.add %A %A" Thread.CurrentThread.ManagedThreadId x)
-//             |> (Observable.subscribeWithCallbacks 
-//                 <| (fun x -> printfn "onNext 1 %A %A" Thread.CurrentThread.ManagedThreadId x)
-//                 <| (fun x -> printfn "onError 1 %A %A" Thread.CurrentThread.ManagedThreadId x)
-//                 <| (fun x -> printfn "onCompleted 1 %A %A" Thread.CurrentThread.ManagedThreadId x)
-//             ) 
-//         printfn "finished setup 1 %A" Thread.CurrentThread.ManagedThreadId
-
-//         // Thread.Sleep 2000
-        
-//         printfn "Observable.add2 %A" Thread.CurrentThread.ManagedThreadId
-//         use anotherdisposable = 
-//             client.Received
-//                 |> (Observable.subscribeWithCallbacks 
-//                     <| (fun x -> printfn "onNext 2 %A %A" Thread.CurrentThread.ManagedThreadId x)
-//                     <| (fun x -> printfn "onError 2 %A %A" Thread.CurrentThread.ManagedThreadId x)
-//                     <| (fun x -> printfn "onCompleted 2 %A %A" Thread.CurrentThread.ManagedThreadId x)
-
-//                 )
-//         printfn "finished setup 2 %A" Thread.CurrentThread.ManagedThreadId
-
-//         Thread.Sleep 60000
-//     ))
-//     |> ignore
-    
-//     0
 
 [<EntryPoint>]
 let main argv =
@@ -102,21 +16,7 @@ let main argv =
     log.Info "FDCConsoleUI is starting! args: %A" argv
         
     Result.success_workflow_with_string_failures {
-        let! host = HostnameData.create "localhost"
-        // let! host = HostnameData.create "p2p.academ.org"
-        // let! host = HostnameData.create "peers.cn.ru"
-        let! port = PortData.create 411
-
-        let connect_info = {
-            ConnectionInfo.host = host
-            ConnectionInfo.port = port 
-        }
-        let listen_info = {
-            ListenInfo.ip = IpAddress.create "127.0.0.1" |> Result.get
-            ListenInfo.port = port 
-        }
-        let! nick = NickData.create "MnZrKk"
-        let pass_maybe = PasswordData.create "dummypass" |> Result.toOption
+        let! settings = Settings.create()
 
         let res = 
             start_queue
@@ -148,8 +48,8 @@ let main argv =
                     loop()
                 loop()
             )
-            <| connect_info
-            <| (nick, pass_maybe)
+            <| settings.hub_connection_info
+            <| (settings.nick, settings.pass_maybe)
 
         return! res
     }
